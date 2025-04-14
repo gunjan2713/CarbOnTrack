b64c0e1fbc33ea2b4cb26c525aa7dc457a35e3b0
Harsh2390 <hs4465@nyu.edu>
2025-04-14 14:53:46 +0400

    minor bug fix.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 import { TripProvider } from "./context/TripContext";
-import "../global.css";
+import "./global.css";
 
 
 export default function RootLayout() {
